5e8134ab52d4cacf1c5fa1b6d205d2d265ed54c3
Mr.X <62176660+InsertX2k@users.noreply.github.com>
2021-07-27 22:10:04 +0200

    Create test2.f90

---
+++ test2.f90
@@ -0,0 +1,11 @@
+program arrays1
+  integer, dimension(10) :: int1
+  integer, dimension(10, 10) :: array2
+  real :: array4(0:9)
+  real :: array5(-5:5)
+
+  print *, ""
+  print *, "array 5 is : ", array5
+  print *, ""
+  print *, "array 4 is : ", array4
+end program arrays1
