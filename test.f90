84e3dcd4701e71bf7673ed04529c527c3dd93c49
Mr.X <62176660+InsertX2k@users.noreply.github.com>
2021-07-27 22:09:08 +0200

    Create test.f90

---
+++ test.f90
@@ -0,0 +1,14 @@
+program variables
+  integer :: x
+  logical :: z
+  print *, "Enter an integer : "
+  read(*,*) x
+  print *, "The integer you entered is : ", x
+  print *, ""
+  print *, ""
+  print *, "Enter a boolean (either .true. or .false.) : "
+  read(*,*) z
+  print *, "The boolean you entered is : ", z
+  print *, "If it shows F then you entered .false., else if it showed T then you entered .true."
+  print *, "The end of the program has been reached, terminating it's process."
+end program variables
